9c7df1397291766322bc6def77f74c21b631e975
claire <86793531+chaireko@users.noreply.github.com>
2025-03-26 07:33:20 -0500

    menu updated

--- project3/src/app/menu/page.tsx
+++ project3/src/app/menu/page.tsx
@@ -3,8 +3,16 @@ import NavBar from "../components/navbar";
 
 export default function Menu() {
   return (
-    <div>
-        <p>hi</p>
+    <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <div className='flex-grow flex flex-col items-center justify-center text-center p-4'>
+          <Image
+              src="/menu.png"
+              alt="Menu"
+              width={900}
+              height={531}
+          />
+        </div>
       
     </div>
   );
