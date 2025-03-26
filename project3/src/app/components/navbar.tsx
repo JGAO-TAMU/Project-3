5c2e6b03f4480b0ba817eb372644fbb20aa5d071
Summer Wong <113924489+sumshiu@users.noreply.github.com>
2025-03-26 00:48:37 -0500

    first draft of cashier side

--- project3/src/app/components/navbar.tsx
+++ project3/src/app/components/navbar.tsx
@@ -6,9 +6,9 @@ export default function Navbar() {
         <div className="navbar bg-base-100 m-10 p-0 flex-none justify-between incline-flex flex items-center">
             <Image src={ShareTeaLogo} alt="Share Tea Logo" width={245} height={65} />
             <div className="flex-row gap-5 items-center flex mr-4 p-2 text-xl">
-            <p>Home</p>
-            <p>Menu</p>
-            <p>Order</p>
+            <a href="">Home</a>
+            <a href="">Menu</a>
+            <a href="">Order</a>
             </div>
         </div>
 )
