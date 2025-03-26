4819ff249f2e5adb19942d93b8e2f7f4b414bbf1
claire <86793531+chaireko@users.noreply.github.com>
2025-03-26 02:24:59 -0500

    landing

---
+++ project3/src/app/menu/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import NavBar from "../components/navbar";
+
+export default function Menu() {
+  return (
+    <div>
+        <p>hi</p>
+      
+    </div>
+  );
+}
